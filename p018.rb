71220d4c830ea44ff24dc251ca6cc55093869e2a
yoggy <yoggy@platina.(none)>
2009-04-03 01:10:12 +0900

    modified:   p018.rb                 とりあえずブルートフォースでProblem18クリアｗ

--- p018.rb
+++ p018.rb
@@ -20,14 +20,13 @@ $str = <<-EOS
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 EOS
 
-
 class Tree
   def initialize
+    # Treeというクラス名だけど、内部では2次元配列でツリーを作成するｗ
     @tree = []
     $str.each{|l|
-      @tree << l.split(/\s+/).inject([]) {|r,i| i.size > 0 ? r << i : r}
+      @tree << l.split.map{|s| s.to_i}
     }
-    pp @tree
   end
 
   def width(y)
@@ -37,12 +36,40 @@ class Tree
   def height
     @tree.size
   end
+
+  def get(x, y)
+    @tree[y][x]
+  end
+
+  # (x,y)の位置から下に[0,1,0,1,....]と辿った数の合計を求める
+  # 0は左下、1は右下へ辿る意味
+  def traverse(x, y, a)
+    return get(x, y) if a == nil || a.size == 0
+    get(x, y) + traverse(x + a[0], y + 1, a[1..-1])
+  end
+  
+  # パスIDに対応する合計を返す
+  def sum(i)
+    b = i.to_s(2)                        # 10->2進数変換
+    b = "0" * (height - 1 - b.size) + b  # 先頭を0埋めする
+    a = b.split(/ */).map{|s| s.to_i}    # 文字列->配列
+    traverse(0, 0, a)
+  end
+
+  def comb_num
+    2 ** (height-1)
+  end
+
+  def max_comb_sum
+    # とりあえずいい方法が思いつかないので、総当たりでw
+    (0...comb_num).inject(-1) {|m, i|
+      s = sum(i)
+      s > m ? s : m
+    }
+  end
 end
 
-# 
-t = Tree.new
-
-
 # 結果の出力
-rv = "not implemented..."
-puts "result = #{rv}"
+t = Tree.new
+puts "result = #{t.max_comb_sum}"
+
