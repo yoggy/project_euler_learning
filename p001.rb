af3a6c5ab8af0d9103122411213729f40289115f
yoggy <yoggy@platina.(none)>
2009-04-02 23:14:18 +0900

    modified:   p018.rb                 eachではなくinjectを使ってみた

--- p001.rb
+++ p001.rb
@@ -3,11 +3,8 @@
 require 'pe'
 desc "1000未満の数のうち、3または5の倍数である数の合計を求める"
 
-total = 0
-(1..999).each{|n|
-  if n%3 == 0 || n%5 == 0
-    total += n
-  end
+total = (1..999).inject(0) {|t, n|
+  n%3 == 0 || n%5 == 0 ? t + n : t
 }
 
 puts "total = #{total}"
