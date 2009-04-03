278ba169070850135af6c3da55dd116acfd22c9a
yoggy <yoggy0@gmail.com>
2009-04-03 23:57:20 +0900

    modified:   p022.rb                 とりあえずProblem 22クリア

--- p022.rb
+++ p022.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/ruby
 require 'pe'
-desc ""
+desc "ファイルにある名前の名前スコアの合計は？"
+
+# 名前1件あたりの名前スコアとは、文字スコアと順番スコアを掛け合わせたもの。
+# 順番スコアは1番目にあれば1、100番目にあれば100
+# 文字スコアとはアルファベットAは1、Bは2...Zは26とし、名前に含むアルファベットの数を合計した値
+# 例:COLINは938番目にある -> 名前スコア = (3 + 15 + 12 + 9 + 14) * 938 = 49714 となる
 
 # ファイルの準備
 url = "http://projecteuler.net/project/names.txt"
@@ -20,16 +25,21 @@ require 'csv'
 names = CSV.read(filename)[0]
 
 # ソート
+total = 0
 names.sort!
 names.each_with_index {|name,i|
-  alpabet_score = 0
+  # 1件あたりの文字スコアを計算
+  alphabet_score = 0
   name.each_byte {|b|
-    alphabet_score += b - 'a'[0] + 1
+    alphabet_score += b - 'A'[0] + 1
   }
+
+  # 名前スコア = 文字スコア*順番スコア
   score = alphabet_score * (i + 1)
-  puts "#{i} : #{name} alphabet_score=#{alphabet_score} score=#{score}"
+  #puts "#{i} : #{name} alphabet_score=#{alphabet_score} score=#{score}"
+
+  total += score
 }
 
 # 結果の出力
-rv = "not implemented..."
-puts "result = #{rv}"
+puts "result = #{total}"
