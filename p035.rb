df1590d2823ad8fa0cb4034dc2752726c5eed1dd
yoggy <yoggy@platina.(none)>
2009-04-11 06:31:01 +0900

    modified:   p035.rb

--- p035.rb
+++ p035.rb
@@ -7,20 +7,39 @@ desc "100万未満で巡回素数はいくつあるか？"
 
 # とりあえず100万未満の素数について総当たりするしか方法ないかなぁ...?
 
+def rotate(n)
+  # とりあえず整数のみ考える
+  a = n.to_s.unpack('c*')
+  return n if a.size <= 1
+
+  x = a.shift
+  a.push x
+  a.pack('c*').to_i
+end
+
+
 # 巡回素数かどうかをチェックする関数
 def check(n)
-  
+  check = true
+  p = n
+  n.to_s.size.times {
+    check = false unless p.prime?
+    p = rotate(p)
+  }
+  check
 end
 
 count = 0
-max = 100
+max = 1000000
 prime = Prime.new
 
 loop {
   p = prime.succ
-  if p >= max break
-
-  count += 1 if check(p)
+  break if p >= max
+  if check(p)
+    puts "hit!! p=#{p}"
+    count += 1
+  end
 }
 
 # 結果の出力
