144054c4e95d33e2a13d7c7e35b8e106848236be
sukedai <sukedai@skd-svr.piecake.com>
2009-04-11 20:16:11 +0900

    new file:   p036.rb

---
+++ p036.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+require 'pe_mylib'
+desc "100万未満で10進数でも2進数でも回文になる数の総和は？"
+
+# 注：先頭が0になる数は含めない
+
+# ここ以下にプログラム書く
+a = 1 + 2 + 3
+
+
+# 結果の出力
+rv = "not implemented..."
+puts "result = #{rv}"
