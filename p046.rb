b7824db18e2eca5af5f57f5fc86ec2d22ecc808b
yoggy <yoggy@platina.(none)>
2009-04-20 12:53:32 +0900

    modified:   p046.rb

--- p046.rb
+++ p046.rb
@@ -19,17 +19,56 @@ desc "素数と二乗した数の2倍で表すことができない奇数の合�
 #
 #   効率
 #     * 奇数の合成数をどうやって列挙するか？
-#     * 素数の列挙
-#     * s*(n**2)の列挙
+#     * Christian Goldbachの予想した数に一致しているかどうかの判定
+#       * (a) 素数の列挙
+#       * (b) s*(n**2)の列挙
+#       * たぶん(b)を順に試していって、残りが素数かどうか判定した方が早そう？
+#         * (a)よりも(b)の方が早く大きな数になる
 #
 #   探索範囲
 #     * 1つ見つかるまで
 #
 
-# ここ以下にプログラム書く
-a = 1 + 2 + 3
+# 素数判定を高速化
+#require 'prime_table'
 
+# 奇数の合成数のスタートは9から
+nc = 9
+
+loop {
+  # 奇数で素数でない物を奇数の合成数とする
+  if nc.prime?
+    nc += 2
+    next
+  end
+
+  # 2*n**2で順に引いていく
+  clean_flag = false
+  n = 1
+  ts = 0
+  loop {
+    ts = 2 * (n ** 2)
+    break if nc <= ts
+
+    #puts "    #{nc} = #{nc - ts} + 2 * (#{n}**2)"
+
+    if (nc - ts).prime?
+      clean_flag = true
+      break
+    end
+
+    n += 1
+  }
+  unless clean_flag 
+    puts "hit!! #{nc}"
+    break
+  end
+
+  nc += 2  # 奇数を探索
+  if (nc-1)%10000 == 0
+    puts "nc = #{nc}"
+  end
+}
 
 # 結果の出力
-rv = "not implemented..."
-puts "result = #{rv}"
+puts "result = #{nc}"
