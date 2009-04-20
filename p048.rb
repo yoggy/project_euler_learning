2eb2b85c5b23274193e4d557d5e854457574cfb5
yoggy <yoggy@platina.(none)>
2009-04-20 14:27:06 +0900

    new file:   p048.rb

---
+++ p048.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+require 'pe_mylib'
+desc "1**1 + 2**2 + 3**3 + .... + 1000**1000の最後の10桁の数字は？"
+
+total = 0
+(1..1000).each{|n|
+  total += n ** n
+}
+
+str = total.to_s
+last10 = strs[s.size - 10, 10]
+
+# 結果の出力
+puts "result = #{last10}"
