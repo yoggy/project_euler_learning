087068ce370a8d4267993a1167617d7241662bd2
yoggy <yoggy0@gmail.com>
2009-04-25 18:58:13 +0900

    modified:   p052.rb

--- p052.rb
+++ p052.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 require 'pe_mylib'
-desc "ここに問題文を書く"
+# Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits in some order.
+desc "xに対して2,3,4,5,6倍してもxと同じ数字を含む一番小さい正の整数は？ただしxと同じ桁に同じ数字があるケースは除く"
 
 # ここ以下にプログラム書く
 a = 1 + 2 + 3
