f544f7343df6d2495bf17763c86f8b2ef45e5a80
yoggy <yoggy@platina.(none)>
2009-04-29 23:24:18 +0900

    modified:   p054.rb

--- p054.rb
+++ p054.rb
@@ -24,8 +24,31 @@ end
 url = "http://projecteuler.net/project/poker.txt"
 text = read_from(url)
 
-pp text
+def eval_cards(cards)
+  pp cards
+
+  # 役と一番大きな数を返す
+  [1,2]
+end
+
+pattern = []
+text.each_line{|l|
+  l.chomp!
+  a = l.split(" ")
+  pattern << [a[0,5], a[5,5]]
+}
+
+count = 0
+pattern.each{|p|
+  v = p.map{|c| eval_cards(c)}
+  if v[0][0] > v[1][0]
+    count += 1
+  elsif v[0][0] == v[1][0]
+    puts "even"
+  else
+    puts "lose"
+  end
+}
 
 # 結果の出力
-rv = "not implemented..."
-puts "result = #{rv}"
+puts "result = #{count}"
