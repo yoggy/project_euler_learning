366458466e493d7f010ef1a0978e5e797dc16257
yoggy <yoggy0@gmail.com>
2009-05-03 15:34:03 +0900

    modified:   p057.rb 	途中。。

--- p057.rb
+++ p057.rb
@@ -10,10 +10,37 @@ desc "ルート2を連分数で表現していく際、1000回までの近似操
 # 
 # 8回目で1393/985になり、はじめて分母の桁よりも分子の桁数が大きくなる
 # 1000回のiteration中でこのような数はいくつあるか？が問題
+#
+# この近似式は次のように変形できる
+#
+#   A = 1 + a
+#   a0 = 1/2
+#   a1 = 1/(2 + a0)  
+#   a2 = 1/(2 + a1)
+#   a3 = 1/(2 + a2)
+#       :
+#       :
+# これで近似していけば1000回ぐらいは余裕？
+#
+# とりあえず189って出たけど正解じゃないみたい。
+# RubyのRationalクラスは約分するからダメなのか？
+#
 
-# 
+limit = 1000
+count = 0
+a_pre = Rational(1, 2) #1回目の値
 
+(2..limit).each{|n|
+  a_now = Rational(1, 2 + a_pre)
+  sqrt2 = 1 + a_now 
+  puts "#{n} = #{sqrt2}"
 
-# 結果の出力
-rv = "not implemented..."
-puts "result = #{rv}"
+  if sqrt2.numerator.digit > sqrt2.denominator.digit
+    puts "hit!! n=#{n}, (#{sqrt2.numerator.digit}/#{sqrt2.denominator.digit}) #{sqrt2} (#{sqrt2.to_f})"
+    count += 1
+  end
+
+  a_pre = a_now
+}
+
+puts "result = #{count}"
