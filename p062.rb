ea21b537f7ddb223118dcec7847d7eca3d89811a
yoggy <yoggy0@gmail.com>
2009-05-05 21:04:22 +0900

    modified:   p062.rb

--- p062.rb
+++ p062.rb
@@ -7,32 +7,20 @@ n = 0
 hit_count = 5
 rv = nil
 
+map = {}
+
 catch(:loop) {
   loop {
     n += 1
     t = n ** 3 
-  
-    a = [t]
-    t.to_a.choice_and_call {|d|
-      next if d[0] == 0
-  
-      # 数を組み立て直す
-      t2 = d.join.to_i
-  
-      #  1/3乗する
-      t3 = t2 ** (1/3.0) #Rationalを指定すると1になってしまうので注意。。
-      ti = t3.round
-      tf = (t3 - ti).abs
-  
-      if tf < 0.000000001 && !a.include?(t2)
-        a << t2
-      end
-    }
-    puts "n=#{n}, t=#{t}, count=#{a.size}"
-  
-    if a.size == hit_count
-      puts "hit!!! n=#{n}, a=#{a.pretty_inspect.chomp}"
-      rv = t
+    sort_t = t.to_a.sort.join #0を含めるので文字列のままにしておく
+    
+    map[sort_t] = [] unless map.key?(sort_t)
+    map[sort_t] << t
+
+    if map[sort_t].size == hit_count
+      puts "hit!!! #{map[sort_t].pretty_inspect}"
+      rv = map[sort_t].sort[0]
       throw :loop
     end
   }
